f9129cd2d1fd03e74a94c14bd2cb525f4b29166d
nabin-bk <nabinbk1255@gmail.com>
2025-12-14 01:38:12 -0800

    first

---
+++ javascript/Object.js
@@ -0,0 +1,10 @@
+const person = {
+name : "madara",
+age : 16 ,
+hobbies : ["gaming","working out","music"]
+
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies[1]);
---
+++ javascript/Square.js
@@ -0,0 +1,5 @@
+const numbers = [1,2,3,4,5];
+const squarednumbers = numbers.map
+(function(num){
+    return num*num;
+});
